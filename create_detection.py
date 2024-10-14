c92c58754713b6ccc4ac6c171ba353754c611fe5
davidus-sk <travel@davidus.sk>
2024-10-13 21:19:03 -0400

    Update create_detection.py

--- create_detection.py
+++ create_detection.py
@@ -60,7 +60,7 @@ for v in config:
 # connecto to sqlite and create DB if it does not exist
 con = sqlite3.connect("/dev/shm/speed.db")
 cur = con.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS detections (time INTEGER, month INTEGER, day INTEGER, hour INTEGER, direction, camera INTEGER, radar, speed, processed INTEGER DEFAULT 0, uploaded INTEGER DEFAULT 0)")
+cur.execute("CREATE TABLE IF NOT EXISTS detections (time INTEGER, month INTEGER, day INTEGER, hour INTEGER, direction, camera INTEGER, radar, speed REAL, processed INTEGER DEFAULT 0, uploaded INTEGER DEFAULT 0)")
 
 # create detection
 data = {"ts": ts, "radar":radar, "speed":speed, "direction":direction, "camera":camera}
@@ -71,43 +71,6 @@ os.mkdir(directory)
 cur.execute(f'INSERT INTO detections VALUES ({ts}, {dt.month}, {dt.day}, {dt.hour}, "{direction}", {camera}, "{radar}", {speed})')
 con.commit()
 con.close()
-
-# give time for the car to pass
-time.sleep(10)
-
-# find images
-best_image = None
-best_score = 0
-best_box = ()
-found_plate = False
-results = []
-
-for name in glob.glob(f"/dev/shm/ffmpeg/{camera}_{ts}*"):
-	if not found_plate or results["predictions"]:
-		results = json.loads(model.infer(name)[0].json())
-
-		for prediction in results["predictions"]:
-			found_plate = True
-			confidence = float(prediction["confidence"])
-			x1 = int(prediction["x"]) - int(prediction["width"]) / 2
-			y1 = int(prediction["y"]) - int(prediction["height"]) / 2
-			x2 = int(prediction["x"]) + int(prediction["width"]) / 2
-			y2 = int(prediction["y"]) + int(prediction["height"]) / 2
-
-			if confidence > best_score:
-				syslog.syslog(syslog.LOG_INFO, f'Found plate with confidence {confidence}.')
-				best_score = confidence
-				best_image = os.path.basename(name)
-				best_box = (x1, y1, x2, y2)
-				shutil.copy2(name, directory)
-
-	# delete original file
-	os.remove(name)
-
-if found_plate:
-	image = Image.open(f"{directory}/{best_image}")
-	cropped_image = image.crop(best_box)
-	cropped_image.save(f"{directory}/plate.jpg")
 
 # post detection to server
 """
