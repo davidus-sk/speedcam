67ca02f8b0e5c7e6f4fe17e2b9635da1d29e337f
davidus-sk <travel@davidus.sk>
2024-10-13 21:16:29 -0400

    Update infer.py

--- infer.py
+++ infer.py
@@ -2,20 +2,21 @@ from inference import get_model
 from PIL import Image
 import glob
 import json
+import sqlite3
 
+# find images
 best_image = None
 best_score = 0
 best_box = ()
+found_plate = False
+results = []
 
-model = get_model(model_id="license-plate-recognition-rxg4e/4")
+for name in glob.glob(f"/dev/shm/ffmpeg/{camera}_{ts}*"):
+	if not found_plate or results["predictions"]:
+		results = json.loads(model.infer(name)[0].json())
 
-for name in glob.glob(f"/dev/shm/ffmpeg/0_1728833558.3241858_*"):
-	results = json.loads(model.infer(name)[0].json())
-
-
-	for prediction in results["predictions"]:
-
-		if "confidence" in prediction:
+		for prediction in results["predictions"]:
+			found_plate = True
 			confidence = float(prediction["confidence"])
 			x1 = int(prediction["x"]) - int(prediction["width"]) / 2
 			y1 = int(prediction["y"]) - int(prediction["height"]) / 2
@@ -23,10 +24,16 @@ for name in glob.glob(f"/dev/shm/ffmpeg/0_1728833558.3241858_*"):
 			y2 = int(prediction["y"]) + int(prediction["height"]) / 2
 
 			if confidence > best_score:
+				syslog.syslog(syslog.LOG_INFO, f'Found plate with confidence {confidence}.')
 				best_score = confidence
-				best_image = name
+				best_image = os.path.basename(name)
 				best_box = (x1, y1, x2, y2)
+				shutil.copy2(name, directory)
 
-image = Image.open(best_image)
-cropped_image = image.crop(best_box)
-cropped_image.save("/tmp/plate.jpg")
+	# delete original file
+	os.remove(name)
+
+if found_plate:
+	image = Image.open(f"{directory}/{best_image}")
+	cropped_image = image.crop(best_box)
+	cropped_image.save(f"{directory}/plate.jpg")
