42d8c70e01134819b23c269b1d3d2c249a036748
davidus-sk <travel@davidus.sk>
2024-10-15 23:42:07 -0400

    Create api.php

---
+++ cloud/api.php
@@ -0,0 +1,16 @@
+<?php
+
+// get posted data
+$camera = $_POST['camera'];
+$speed = $_POST['speed'];
+$ts = $_POST['ts'];
+$radar = $_POST['radar'];
+$plate = $_FILES['plate'];
+$car = $_FILES['car'];
+
+// save data
+if (!empty($camera) && !empty($speed) && !empty($ts) && !empty($radar)) {
+  
+}//if
+
+echo "ERROR";
