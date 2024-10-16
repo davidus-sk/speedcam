67af48f75191d76f7b8ccd483771afd91152b42c
davidus-sk <travel@davidus.sk>
2024-10-16 00:32:08 -0400

    Update DB.php

--- cloud/DB.php
+++ cloud/DB.php
@@ -93,6 +93,6 @@ class DB {
 	 */
 	public function createSchemas() {
 		// create table detections
-		$this->conn->exec('CREATE TABLE IF NOT EXISTS detections (time, month INTEGER, day INTEGER, hour INTEGER, year INTEGER, camera INTEGER, radar, speed, plate, image1, image2)');
+		$this->conn->exec('CREATE TABLE IF NOT EXISTS detections (time, month INTEGER, day INTEGER, hour INTEGER, year INTEGER, camera INTEGER, radar, speed REAL, plate, image1, image2)');
 	}//function
 }//class
