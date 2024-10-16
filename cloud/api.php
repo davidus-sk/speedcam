07a42c3be774500b4b5862de5c6d98163d0907df
davidus-sk <travel@davidus.sk>
2024-10-16 01:38:52 -0400

    Update api.php

--- cloud/api.php
+++ cloud/api.php
@@ -8,6 +8,8 @@ $camera = empty($_POST['camera']) ? null : (int)$_POST['camera'];
 $speed = empty($_POST['speed']) ? null : (float)$_POST['speed'];
 $ts = empty($_POST['ts']) ? null : $_POST['ts'];
 $radar = empty($_POST['radar']) ? null : $_POST['radar'];
+$direction = empty($_POST['direction']) ? null : $_POST['direction'];
+$location = empty($_POST['location']) ? null : $_POST['location'];
 $plate = empty($_FILES['plate']) ? null : $_FILES['plate'];
 $car = empty($_FILES['car']) ? null : $_FILES['car'];
 
@@ -22,8 +24,7 @@ if (!is_null($camera) && !is_null($speed) && !is_null($ts) && !is_null($radar)) 
   $db->createSchemas();
 
   // store data
-  // (ts TEXT, month INTEGER, day INTEGER, hour INTEGER, year INTEGER, camera INTEGER, radar, speed REAL, plate TEXT, image1 TEXT, image2 TEXT)
-  $db->query('INSERT INTO detections VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [$ts, (int)$dt->format("n"), (int)$dt->format("j"), (int)$dt->format("G"), (int)$dt->format("Y"), $camera, $radar, $speed, null, null, null]);
+  $db->query('INSERT INTO detections VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [$ts, (int)$dt->format("n"), (int)$dt->format("j"), (int)$dt->format("G"), (int)$dt->format("Y"), $camera, $radar, $speed, $direction, $location, null, null, null]);
 
   echo "OK: {$db->lastInsertId()}";
   exit();
