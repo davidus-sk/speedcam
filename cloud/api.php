7ab06c0e781bb76ad594d6b958fea694f60f8c98
davidus-sk <travel@davidus.sk>
2024-10-16 00:41:12 -0400

    Update api.php

--- cloud/api.php
+++ cloud/api.php
@@ -1,16 +1,30 @@
 <?php
+// include DB
+include 'DB.php';
+$db = new DB('speed_cloud.db');
 
 // get posted data
-$camera = $_POST['camera'];
-$speed = $_POST['speed'];
-$ts = $_POST['ts'];
-$radar = $_POST['radar'];
-$plate = $_FILES['plate'];
-$car = $_FILES['car'];
+$camera = empty($_POST['camera']) ? null : (int)$_POST['camera'];
+$speed = empty($_POST['speed']) ? null : (float)$_POST['speed'];
+$ts = empty($_POST['ts']) ? null : $_POST['ts'];
+$radar = empty($_POST['radar']) ? null : $_POST['radar'];
+$plate = empty($_FILES['plate']) ? null : $_FILES['plate'];
+$car = empty($_FILES['car']) ? null : $_FILES['car'];
+
+// date time
+$dt = new DateTime("@{$timestamp}", new DateTimeZone("America/New_York"));
 
 // save data
-if (!empty($camera) && !empty($speed) && !empty($ts) && !empty($radar)) {
-  
+if (!is_null($camera) && !is_null($speed) && !is_null($ts) && !is_null($radar)) {
+  // init the schema
+  $db->createSchemas();
+
+  // store data
+  // (time, month INTEGER, day INTEGER, hour INTEGER, year INTEGER, camera INTEGER, radar, speed REAL, plate, image1, image2)
+  $db->query('INSERT INTO detections VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [$ts, (int)$dt->format("n"), (int)$dt->format("j"), (int)$dt->format("G"), (int)$dt->format("Y"), $camera, $radar, $speed, null, null, null]);
+
+  echo "OK: {$db->lastInsertId()}";
+  exit();
 }//if
 
-echo "ERROR";
+echo "ERROR: {$camera}, {$speed}, {$ts}, {$radar}";
