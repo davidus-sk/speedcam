0d757a5a4dd1176326907066b9c52110c0e3a27a
davidus-sk <travel@davidus.sk>
2024-10-16 21:42:20 -0400

    Update index.php

--- cloud/index.php
+++ cloud/index.php
@@ -1,4 +1,23 @@
 <?php
+include 'DB.php';
+$db = new DB('speed_cloud.db');
+
+// get counts today and yesterday
+$count_today_r = $db->fetchResult('SELECT hour, count(time) as cnt FROM detections WHERE month=? AND day=? AND year=? GROUP BY hour', [date('n'), date('j'), date('Y')]);
+$count_yesterday_r = $db->fetchResult('SELECT hour, count(time) as cnt FROM detections WHERE month=? AND day=? AND year=? GROUP BY hour', [date('n', time() - 86400), date('j', time() - 86400), date('Y, time() - 86400)]);
+
+
+// get data into arrays
+$count_today = [];
+$count_yesterday = [];
+
+while ($row = $count_today_r->fetchArray()) {
+	$count_today[$row['hour']] = $row['cnt'];
+}//while
+
+while ($row = $count_yesterday_r->fetchArray()) {
+	$count_yesterday[$row['hour']] = $row['cnt'];
+}//while
 
 ?>
 <!doctype html>
@@ -26,9 +45,7 @@
               Speeding Detections by Hour (today)
             </div>
             <div class="card-body">
-              <h5 class="card-title">Special title treatment</h5>
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
+              <canvas id="g_cnt_today" style="width:100%"></canvas>
             </div>
           </div>
         </div>
@@ -92,6 +109,41 @@
       </div>
   </div>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
+
+  	<script>
+  	const count_today_x = <?php echo json_encode(array_keys($count_today)); ?>;
+  	const count_today_y = <?php echo json_encode(array_values($count_today)); ?>;
+  	const count_yesterday_y = <?php echo json_encode(array_values($count_yesterday)); ?>;
+  
+    new Chart("g_count_today", {
+      type: "bar",
+      data: {
+        labels: count_today_x,
+        datasets: [{
+          data: count_today_y,
+          backgroundColor: "#2196F3"
+        },
+    	      {
+          data: count_yesterday_y,
+          backgroundColor: "#8acbff"
+        }]
+      },
+      options: {
+        legend: {display: false},
+        title: {
+          display: false,
+        },
+        scales: {
+          xAxes: [{
+            gridLines: {
+              display: false // Disables grid lines on the x-axis
+            }
+          }],
+        }
+      }
+    });
+    </script>
   </body>
 </html>
