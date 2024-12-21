f44c44a6947f87f6815427df0c165645c005555d
davidus-sk <travel@davidus.sk>
2024-12-21 13:21:42 -0500

    Update index.php

--- cloud/index.php
+++ cloud/index.php
@@ -6,84 +6,25 @@ $db = new DB('speed_cloud.db');
 // get global vars
 $location = empty($_GET['l']) ? 1 : (int)$_GET['l'];
 $week = $_GET['week'];
+$_route = !empty($_GET['r']) ? trim($_GET['r']) : 'home';
 
-if (!empty($week) && ($week != date('W'))) {
-	$day_offset = ($week - 1) * 7;
-	$dayy_offset = ($week - 2) * 7;
-	
-	$dtw = new DateTime(date('Y-01-01 00:00:00'), new DateTimeZone("America/New_York"));
-	$dtyw = new DateTime(date('Y-01-01 00:00:00'), new DateTimeZone("America/New_York"));
+// start output buffer
+ob_start();
 
-	$dtw->modify("+{$day_offset} days");
-	$dtyw->modify("+{$dayy_offset} days");
-
-	// get counts week
-	$count_today_r = $db->fetchResult('SELECT hour, count(ts) as cnt FROM detections WHERE ts >= ? AND ts < ? GROUP BY hour', [$dtw->getTimestamp(), $dtw->getTimestamp() + 604800]);
-	$count_yesterday_r = $db->fetchResult('SELECT hour, count(ts) as cnt FROM detections WHERE 1=0');
-} else {
-	$dtw = new DateTime('Monday this week 00:00:00', new DateTimeZone("America/New_York"));
-	$dtyw = new DateTime('Monday previous week 00:00:00', new DateTimeZone("America/New_York"));
-
-	$dt = new DateTime('today 00:00:00', new DateTimeZone("America/New_York"));
-	$dty = new DateTime('yesterday 00:00:00', new DateTimeZone("America/New_York"));
-
-	// get counts today and yesterday
-	$count_today_r = $db->fetchResult('SELECT hour, count(ts) as cnt FROM detections WHERE ts >= ? AND ts < ? GROUP BY hour', [$dt->getTimestamp(), $dt->getTimestamp() + 86400]);
-	$count_yesterday_r = $db->fetchResult('SELECT hour, count(ts) as cnt FROM detections WHERE ts >= ? AND ts < ? GROUP BY hour', [$dty->getTimestamp(), $dty->getTimestamp() + 86400]);
+if( file_exists($_route . '.php') )
+{
+        // If all good, load the page
+        include '$_route . '.php';
 }
+else
+{
+        // Can't find the page, show error
+        include '404.php';
+}//if
 
-// get counts for this and last week
-$count_week_r = $db->fetchResult('SELECT * FROM detections WHERE ts >= ? AND ts < ? ORDER BY ts DESC', [$dtw->getTimestamp(), $dtw->getTimestamp()+604800]);
-$count_yesterweek_r = $db->fetchResult('SELECT * FROM detections WHERE ts >= ? AND ts < ?', [$dtyw->getTimestamp(), $dtyw->getTimestamp()+604800]);
 
-// get data into arrays
-$top_speed = 0;
-$speed_buckets = ['30' => 0, '40' => 0, '50' => 0, '60' => 0];
-$count_total = 0;
-$count_today = [];
-$count_yesterday = [];
-$count_week = ['Monday' => 0, 'Tuesday' => 0, 'Wednesday' => 0, 'Thursday' => 0, 'Friday' => 0, 'Saturday' => 0, 'Sunday' => 0];
-$count_yesterweek = ['Monday' => 0, 'Tuesday' => 0, 'Wednesday' => 0, 'Thursday' => 0, 'Friday' => 0, 'Saturday' => 0, 'Sunday' => 0];
-$average_week_i = ['Monday' => [0,0], 'Tuesday' => [0,0], 'Wednesday' => [0,0], 'Thursday' => [0,0], 'Friday' => [0,0], 'Saturday' => [0,0], 'Sunday' => [0,0]];
-$average_week = ['Monday' => 0, 'Tuesday' => 0, 'Wednesday' => 0, 'Thursday' => 0, 'Friday' => 0, 'Saturday' => 0, 'Sunday' => 0];
-
-while ($row = $count_today_r->fetchArray()) {
-	$count_today[$row['hour']] = $row['cnt'];
-}//while
-
-while ($row = $count_yesterday_r->fetchArray()) {
-	$count_yesterday[$row['hour']] = $row['cnt'];
-}//while
-
-while($row = $count_week_r->fetchArray()) {
-	$d = new DateTime('now', new DateTimeZone("America/New_York"));
-	$d->setTimestamp($row['ts']);
-	$count_week[$d->format('l')]++;
-	$count_total++;
-	$top_speed = $row['speed'] > $top_speed ? $row['speed'] : $top_speed;
-	$speed_range = floor($row['speed'] * 0.621372 / 10) * 10;
-	$speed_buckets[$speed_range]++;
-	$average_week_i[$d->format('l')][0] = $average_week_i[$d->format('l')][0] + ($row['speed'] * 0.621372);
-	$average_week_i[$d->format('l')][1]++;
-	$average_week[$d->format('l')] = round($average_week_i[$d->format('l')][0] / $average_week_i[$d->format('l')][1]);
-}//while
-
-while($row = $count_yesterweek_r->fetchArray()) {
-	$d = new DateTime('now', new DateTimeZone("America/New_York"));
-	$d->setTimestamp($row['ts']);
-	$count_yesterweek[$d->format('l')]++;
-}//while
-
-// pad arrays
-for ($i = 0; $i < 24; $i++) {
-	$count_today[$i] = isset($count_today[$i]) ? $count_today[$i] : 0;
-	$count_yesterday[$i] = isset($count_yesterday[$i]) ? $count_yesterday[$i] : 0;
-}//for
-
-// sort the padded arrays
-ksort($count_today);
-ksort($count_yesterday);
-
+// store output
+$_content = ob_get_clean();
 ?>
 <!doctype html>
 <html lang="en">
@@ -157,125 +98,7 @@ ksort($count_yesterday);
 				</div>
 			</div>
 
-			<div class="row mb-4">
-				<div class="col-md-12">
-					<div class="card">
-						<div class="card-header">
-							<b>Speeding Detections by Hour</b> 
-							<span class="float-end"><a href="/list.php?tf=day">List offenders</a> | <a href="/download.php?tf=day">Download CSV</a></span>
-						</div>
-						<div class="card-body">
-							<canvas id="g_count_today" style="width:100%"></canvas>
-						</div>
-						<div class="card-footer">
-							<?php if (empty($week) || $week == date('W')) { ?>
-							<span class="badge text-bg-primary" style="background-color:#2196F3 !important"><?php echo $dt->format('l'); ?></span> and <span class="badge text-bg-primary" style="background-color:#8acbff !important"><?php echo $dty->format('l'); ?></span>
-							<?php } else { ?>
-							<span class="badge text-bg-primary" style="background-color:#2196F3 !important">Week <?php echo $dtw->format('W'); ?></span>
-							<?php } ?>
-						</div>
-					</div>
-				</div>
-			</div>
-
-			<div class="row mb-4">
-				<div class="col-md-12">
-					<div class="card">
-						<div class="card-header">
-							<b>Speeding Detections by Day</b>
-							<a href="/download.php?tf=week" class="float-end">Download CSV</a>
-						</div>
-						<div class="card-body">
-							<canvas id="g_count_week" style="width:100%"></canvas>
-						</div>
-						<div class="card-footer">
-							<span class="badge text-bg-primary" style="background-color:#2196F3 !important">Week <?php echo $dtw->format('W'); ?></span> and <span class="badge text-bg-primary" style="background-color:#8acbff !important">Week <?php echo $dtyw->format('W'); ?></span>
-						</div>
-					</div>
-				</div>
-			</div>
-
-			<div class="row mb-4">
-				<div class="col-md-6">
-					<div class="card">
-						<div class="card-header">
-							<b>Speed Range Distribution</b>
-						</div>
-						<div class="card-body">
-							<canvas id="g_speed_range" style="width:100%"></canvas>
-						</div>
-						<div class="card-footer">
-							<span class="badge text-bg-primary" style="background-color:#00d700 !important">30-39 mph</span>,
-							<span class="badge text-bg-primary" style="background-color:#0000FF !important">40-49 mph</span>,
-							<span class="badge text-bg-primary" style="background-color:#f3c50f !important">50-59 mph</span>, and
-							<span class="badge text-bg-primary" style="background-color:#FF0000 !important">60+ mph</span>
-						</div>
-					</div>
-				</div>
-
-				<div class="col-md-6">
-					<div class="card">
-						<div class="card-header">
-							<b>Average Speed by Day</b>
-						</div>
-						<div class="card-body">
-							<canvas id="g_speed_average" style="width:100%"></canvas>
-						</div>
-						<div class="card-footer">
-							<span class="badge text-bg-primary" style="background-color:#2196F3 !important">Week <?php echo $dtw->format('W'); ?></span>
-						</div>
-					</div>
-				</div>
-			</div>
-
-			<div class="row mb-4">
-				<div class="col-md-12">
-					<div class="card">
-						<div class="card-header">
-							<b>Last 20 Detections</b>
-						</div>
-						<div class="card-body">
-							<div class="table-responsive">
-								<table class="table">
-									<tr>
-										<th>Date</th>
-										<th>Time</th>
-										<th>Speed</th>
-										<th>Direction</th>
-										<th>Image</th>
-										<th>Plate</th>
-									</tr>
-		
-									<?php
-									$limit = 20;
-									while($row = $count_week_r->fetchArray()) {
-										$limit--;
-										$d = new DateTime('now', new DateTimeZone("America/New_York"));
-										$d->setTimestamp($row['ts']);
-									?>
-		
-									<tr>
-										<td><?php echo $d->format('m/d/Y'); ?></td>
-										<td><?php echo $d->format('h:i A'); ?></td>
-										<td><?php echo floor($row['speed'] * 0.621372); ?> mph</td>
-										<td><?php echo $row['direction']; ?></td>
-										<td><?php echo empty($row['plate']) ? '-' : $row['plate']; ?></td>
-										<td><?php echo empty($row['image1']) ? '-' : $row['image1']; ?></td>
-									</tr>
-		
-									<?php
-										if ($limit == 0) {
-											break;
-										}//if
-									}//while
-									?>
-		
-								</table>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+			<?php echo $_content; ?>
 
 			<div class="mb-4 rounded bg-body-tertiary">
 				<div class="row">
@@ -284,135 +107,6 @@ ksort($count_yesterday);
 			</div>
 		</div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
-
-  	<script>
-  	const count_today_x = <?php echo json_encode(array_keys($count_today)); ?>;
-  	const count_today_y = <?php echo json_encode(array_values($count_today)); ?>;
-  	const count_yesterday_y = <?php echo json_encode(array_values($count_yesterday)); ?>;
-
-  	const count_week_x = <?php echo json_encode(array_keys($count_week)); ?>;
-  	const count_week_y = <?php echo json_encode(array_values($count_week)); ?>;
-  	const count_yesterweek_y = <?php echo json_encode(array_values($count_yesterweek)); ?>;
-	const average_week_y = <?php echo json_encode(array_values($average_week)); ?>;
-  
-    new Chart("g_count_today", {
-      type: "bar",
-      data: {
-        labels: count_today_x,
-        datasets: [{
-          data: count_today_y,
-          backgroundColor: "#2196F3"
-        },
-    	      {
-          data: count_yesterday_y,
-          backgroundColor: "#8acbff"
-        }]
-      },
-      options: {
-        legend: {display: false},
-        title: {
-          display: false,
-        },
-        scales: {
-          xAxes: [{
-            gridLines: {
-              display: false // Disables grid lines on the x-axis
-            }
-          }],
-        }
-      }
-    });
-
-    new Chart("g_count_week", {
-      type: "bar",
-      data: {
-        labels: count_week_x,
-        datasets: [{
-          data: count_week_y,
-          backgroundColor: "#2196F3"
-        },
-    	      {
-          data: count_yesterweek_y,
-          backgroundColor: "#8acbff"
-        }]
-      },
-      options: {
-        legend: {display: false},
-        title: {
-          display: false,
-        },
-        scales: {
-          xAxes: [{
-            gridLines: {
-              display: false // Disables grid lines on the x-axis
-            }
-          }],
-        }
-      }
-    });
-
-	new Chart("g_speed_range", {
-		type: "pie",
-		data: {
-			labels: <?php echo json_encode(array_keys($speed_buckets)); ?>,
-			datasets: [{
-			data: <?php echo json_encode(array_values($speed_buckets)); ?>,
-			backgroundColor: [
-				'#00d700', '#0000FF', '#f3c50f', '#FF0000'
-			],
-			}]
-		},
-		options: {
-			legend: {display: false},
-			title: {display: false},
-			scales: { },
-    plugins: {
-      datalabels: {
-        display: true,
-        formatter: (val, ctx) => {
-          // Grab the label for this value
-          const label = ctx.chart.data.labels[ctx.dataIndex];
-
-          // Format the number with 2 decimal places
-          const formattedVal = Intl.NumberFormat('en-US', {
-            minimumFractionDigits: 2,
-          }).format(val);
-
-          // Put them together
-          return `${label}: ${formattedVal}`;
-        },
-        color: '#fff',
-        backgroundColor: '#404040',
-      },
-    },
-		}
-	});
-
-    new Chart("g_speed_average", {
-      type: "bar",
-      data: {
-        labels: count_week_x,
-        datasets: [{
-          data: average_week_y,
-          backgroundColor: "#2196F3"
-        }]
-      },
-      options: {
-        legend: {display: false},
-        title: {
-          display: false,
-        },
-        scales: {
-          xAxes: [{
-            gridLines: {
-              display: false // Disables grid lines on the x-axis
-            }
-          }],
-        }
-      }
-    });
-	</script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 	</body>
 </html>
