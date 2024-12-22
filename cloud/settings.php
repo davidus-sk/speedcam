8b9c407e1c51eba94d415e3f4b14933fafe387c7
davidus-sk <travel@davidus.sk>
2024-12-22 01:12:38 -0500

    Update settings.php

--- cloud/settings.php
+++ cloud/settings.php
@@ -17,12 +17,20 @@ while ($row = $locations_r->fetchArray()) {
 					</div>
 					<div class="col-md-6">
 						<div class="input-group">
-							<input type="text" class="form-control" name="speedlimit[<?php echo $row['rowid']; ?>]" value="<?php echo $row['speedlimit']; ?>" />
+							<input type="number" class="form-control" name="speedlimit[<?php echo $row['rowid']; ?>]" value="<?php echo $row['speedlimit']; ?>" />
 							<span class="input-group-text" id="basic-addon2">mph</span>
 						</div>
 					</div>
 				</div>
 			</div>
+		</div>
+	</div>
+</div>
+
+<div class="mb-4 rounded bg-body-secondary">
+	<div class="row">
+		<div class="col-md-12">
+			<p class="p-3 m-0 text-center"><button type="submit" class="btn btn-primary">Save Settings</button></p>
 		</div>
 	</div>
 </div>
