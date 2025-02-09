7f5a98809f39a47053fffb2b62e884a0441c63c8
David <david@luceon.us>
2025-02-09 00:08:19 -0500

    vms

--- edge/vms/vms_record.php
+++ edge/vms/vms_record.php
@@ -28,6 +28,6 @@ foreach ($cams as $cam) {
 	
 	if (empty($pid)) {
 		syslog(LOG_INFO, "Starting capture for {$cam['url']}.");
-		`/usr/bin/ffmpeg -hide_banner -rtsp_transport udp -fflags discardcorrupt -flags low_delay -r 15 -i {$cam['url']} -f segment -segment_time 5 -reset_timestamps 1 -strftime 1 -c copy "/data/vms/{$cam['camera']}_%Y-%m-%d_%H-%M-%S.mp4" 2> /dev/null &`;
+		`/usr/bin/ffmpeg -hide_banner -rtsp_transport udp -fflags discardcorrupt -flags low_delay -r 15 -i {$cam['url']} -f segment -segment_time 5 -reset_timestamps 1 -strftime 1 -c copy "/data/vms/{$cam['camera']}_%Y-%m-%d_%H-%M-%S.mp4" > /dev/null 2>&1 &`;
 	}//if
 }//foreach
