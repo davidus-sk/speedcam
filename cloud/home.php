4263933c6d3dc33e1b15c3f067c3bb2c8d3263da
root <travel@davidus.sk>
2025-02-16 10:59:37 -0500

    fix

--- cloud/home.php
+++ cloud/home.php
@@ -158,7 +158,7 @@ ksort($count_yesterday);
 										<td><?php echo $d->format('h:i A'); ?></td>
 										<td><?php echo floor($row['speed'] * 0.621372); ?> mph</td>
 										<td><?php echo $row['direction']; ?></td>
-										<td></td>
+										<td><?php echo empty($row['video']) ? '-' : $row['video']; ?></td>
 										<td><?php echo empty($row['image']) ? '-' : $row['image']; ?></td>
 										<td><?php echo empty($row['plate']) ? '-' : $row['plate']; ?></td>
 									</tr>
